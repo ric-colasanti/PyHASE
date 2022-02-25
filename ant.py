b451a31337f5298d368ec173ab3f7579e6129407
Dr Ricardo Colasanti <developer.colasanti@gmail.com>
2022-02-25 17:40:29 +0000

    upsize

--- ant.py
+++ ant.py
@@ -8,12 +8,9 @@ HEIGHT = 600
 # Create a new simulation of grid size 80 X 80 cells
 # Set the variable in each cell of as "resource" and set value to 0
 #world = ABM.World(30, ["resource"],["rnd"])
-world = ABM.World(120, ["state"],[0],n_type=4)
-#set center cell resource to 500
-#add agent to top lefthand corner
-world.addAgent(20,20)
-#world.setState(1, 1, "state", 1)
-world.update()
+world = ABM.World(180, ["state"],[0],n_type=4)
+
+world.addAgent(60,60)
 
 
 
@@ -35,13 +32,9 @@ def update():
             agent.left()
         #agent.moveRandom()
 
-    #world.update()
     #Draw the world with squares that have their "live" variable set to 1
     ABM.draw(screen, world, "state", 1,discreet=True)
 
-
-    # Uncomment the line below to output total resource
-    #print((world.sumAll("resource"),0))
 
 #Start the simulation
 ABM.go()
