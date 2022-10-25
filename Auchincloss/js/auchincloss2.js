3cebb7ceee0f0655d19b09bd67736d5128cdfece
= <=>
2022-10-25 19:03:44 +0100

    Choice based on price

--- Auchincloss/js/auchincloss2.js
+++ Auchincloss/js/auchincloss2.js
@@ -43,12 +43,18 @@ var distance = function () {
             let shop = shops[j]
             let xdif = Math.abs(person.xPos() - shop.xPos())
             let ydif = Math.abs(person.yPos() - shop.yPos())
-            let distance =Math.sqrt(Math.abs(xdif * xdif + ydif * ydif))
-            if(person.sClass==0){
-                distance = distance*distance
+            let distance = Math.sqrt(Math.abs(xdif * xdif + ydif * ydif))
+            if (person.sClass == 0) {
+                distance = distance * distance
             }
-            distance = 1/distance
+            distance = 1 / distance
             total += distance
+            if ((person.sClass == 1) && (shop.sClass == 1)) {
+                total += 0.9
+            }
+            if ((person.sClass == 0) && (shop.sClass == 1)) {
+                total *= 0.1
+            }
             //console.log(distance,total);
             person.shops.push(shop)
             person.shopsScore.push(total)
@@ -60,7 +66,7 @@ var distance = function () {
 var chooseShop = function () {
     for (let i = 0; i < shops.length; i++) {
         const shop = shops[i];
-        shop.customers=0
+        shop.customers = 0
     }
     for (let i = 0; i < population.length; i++) {
         let person = population[i];
@@ -69,7 +75,12 @@ var chooseShop = function () {
         while (choice > person.shopsScore[j]) {
             j++
         }
-        person.color = person.shops[j].color
+        if(person.shops[j].sClass==0){
+            person.color = "darkblue"//person.shops[j].color
+        }else{
+            person.color = "darkred"
+        }
+        
         person.shops[j].customers++;
     }
 }
@@ -82,14 +93,15 @@ var setup = function () {
     for (i = 0; i < size * size; i++) {
         var home = new Home();
         var sclass = 0
-        if (rndInt(2) == 0) {
+        //if (rndInt(2) == 0) {
+        if (i < size * size / 2) {
             home.color = "#dddddd"
-  
+
         } else {
             home.color = "#aaaaaa"
-            sclass=1
+            sclass = 1
         }
-        if (Math.random() < 0.005) {
+        if (Math.random() < 0.01) {
             agent = new Shop(rndInt(2), 0, i);
             agent.color = "rgb(" + rndInt(255) + "," + rndInt(255) + "," + rndInt(255) + ")"
             shops.push(agent)
@@ -110,26 +122,45 @@ var draw = function () {
     });
     for (let i = 0; i < population.length; i++) {
         const person = population[i];
-        caCanvas.drawCircle(person.xPos(), person.yPos(), person.color,"black", 4, 1); 
+        caCanvas.drawCircle(person.xPos(), person.yPos(), person.color, "black", 4, 1);
     }
     for (let i = 0; i < shops.length; i++) {
         const shop = shops[i];
         color = "darkred"
-        if(shop.sClass==1){
+        if (shop.sClass == 1) {
             color = "darkblue"
         }
-        caCanvas.drawCircle(shop.xPos(), shop.yPos(), shop.color,color, -8, 24);
+        caCanvas.drawCircle(shop.xPos(), shop.yPos(), shop.color, color, -8, 24);
     }
     caCanvas.update("canvas");
 }
 
+var count = 0
 var update = function () {
+    if (count >= 10) {
+        var min = 10000
+        let lshop = null
+        for (let i = 0; i < shops.length; i++) {
+            const shop = shops[i];
+            if (shop.customers < min) {
+                lshop = shop
+                min = shop.customers
+            }
+        }
+        if (lshop.sClass == 0 ){
+            lshop.sClass=1
+        }else{
+            lshop.sClass=0
+        }
+        distance()
+        count=0
+    }
     chooseShop()
     draw()
-
+    count++
     setTimeout(function () {
         window.requestAnimationFrame(update);
-    }, 500);
+    }, 50);
 
 };
 
